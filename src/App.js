838a9c31ed387e9879a9cd61d73d192b36156648
aimalhuma61 <66478525+aimalhuma61@users.noreply.github.com>
2020-06-25 09:01:54 +0500

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React,{useEffect, useState} from 'react';
+
+import './App.css';
+
+function App() {
+  const [repos,setRepos]=useState([]);
+useEffect(()=>{
+  async function getReposit(){
+  
+
+      const respnse= await fetch("https://api.github.com/users/kiranhuma/repos")
+       const data=await respnse.json();
+       console.log(data);
+       setRepos(data);
+  }
+ 
+  getReposit();
+
+  //  "https"://api.github.com/users/muhammadmohsin/repos"
+    // fetch('https://jsonplaceholder.typicode.com/posts/1')
+    // .then(response => response.json()) ////".then" mean promise
+    // .then(json => {
+    //   setdata(json);
+  //  })
+
+   },[ ])
+  return (
+   <div className="App">
+     <h1>All repositries</h1>
+     <ul>
+       {repos.map((repoObj, ind) => {
+        return(<li key={ind}>{repoObj.name}
+   
+        </li>)
+      })}
+     </ul>
+    </div>
+  );
+}
+
+export default App;
